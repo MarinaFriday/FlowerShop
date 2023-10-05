b118660ab1643799c8856aa1b466adfce73b02bd
MarinaFriday <kokorina1995@list.ru>
2023-10-05 20:49:39 +0400

    Create ProviderController, ShipmentController

---
+++ FlowerShop/FlowerShop/webapi/Controllers/ShipmentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+using webapi.Data;
+using webapi.Models.Flowers;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipmentController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ShipmentController(DataContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Shipment>>> GetShipments() {
+            return await _context.Shipments.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Shipment>> GetShipment(int id) { 
+            
+        }
+    }
+}
